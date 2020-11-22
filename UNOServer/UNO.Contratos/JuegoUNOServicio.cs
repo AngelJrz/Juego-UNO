4fcee0c2406edc96e1937f5e7c116cef6c6a8b67
SammyGCh <53235299+SammyGCh@users.noreply.github.com>
2020-11-21 19:35:21 -0600

    Creacion del servicio para el juego
    Se crearon las interfaces para el registro de un jugador

---
+++ UNOServer/UNO.Contratos/JuegoUNOServicio.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using UNO.Contratos.RegistrarJugador;
+using UNO.Dominio;
+
+namespace UNO.Contratos
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single, ConcurrencyMode = ConcurrencyMode.Multiple)]
+    public partial class JuegoUNOServicio : IRegistrarJugador
+    {
+        public void RegistrarJugador(Jugador jugador)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void VerificarClave(string clave)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UNOServer/UNO.Contratos/RegistrarJugador/ResultadoRegistro.cs
@@ -0,0 +1,10 @@
+﻿namespace UNO.Contratos.RegistrarJugador
+{
+    public enum ResultadoRegistro
+    {
+        RegistroExitoso = 1,
+        ErrorRegistro = 2,
+        UsuarioYaExiste = 3,
+        CorreoYaExiste = 4
+    }
+}
