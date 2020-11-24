17624101144ad07f2d2ba0bdb0c3547ba78ffa65
AngelJuarezGarcia <inscripangel@hotmail.com>
2020-11-23 21:33:23 -0600

    Nuevas ventanas agregadas
    Se agregaron las ventanas de:
    -Creacion de sala
    -Menu principal
    -Unirse a sala

---
+++ UNOGui/Ventanas/CreacionDeSala.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UNOGui.Ventanas
+{
+    /// <summary>
+    /// Lógica de interacción para CreacionDeSala.xaml
+    /// </summary>
+    public partial class CreacionDeSala : Window
+    {
+        public CreacionDeSala()
+        {
+            InitializeComponent();
+        }
+
+        private void Cancelar(object sender, RoutedEventArgs e)
+        {
+            MenuPrincipal menu = new MenuPrincipal();
+            menu.Show();
+
+            this.Close();
+        }
+
+        private void CrearSala(object sender, RoutedEventArgs e)
+        {
+            if (CamposCompletos())
+            {
+                MessageBox.Show("Creaste una sala");
+            }
+            else
+            {
+                MessageBox.Show("Los campos estan incompletos");
+            }
+        }
+
+        private bool CamposCompletos()
+        {
+            return (nombreSala.Text.Trim() != "" && contrasenia.Password.Trim() != "");
+        }
+    }
+}
---
+++ UNOGui/Ventanas/MenuPrincipal.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UNOGui.Ventanas
+{
+    /// <summary>
+    /// Lógica de interacción para MenuPrincipal.xaml
+    /// </summary>
+    public partial class MenuPrincipal : Window
+    {
+        public MenuPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void CerrarSecion(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void IrAUnirseASala(object sender, RoutedEventArgs e)
+        {
+            UnirseASala unirseSala = new UnirseASala();
+            unirseSala.Show();
+
+            this.Close();
+        }
+
+        private void IrACrearSala(object sender, RoutedEventArgs e)
+        {
+            CreacionDeSala nuevasala = new CreacionDeSala();
+            nuevasala.Show();
+
+            this.Close();
+        }
+    }
+}
