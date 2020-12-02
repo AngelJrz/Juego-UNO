5a7671267caf4e0765aee7e43b357a497d60796e
AngelJuarezGarcia <inscripangel@hotmail.com>
2020-12-02 14:16:34 -0600

    Creación de interfaces en servidor

---
+++ UNOServer/UNO.Contratos/AdministrarJugador/IRecuperarCuenta.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UNO.Contratos.AdministrarJugador
+{
+    [ServiceContract(CallbackContract = typeof(IRecuperarCuentaCallback))]
+    public interface IRecuperarCuenta
+    {
+        [OperationContract(IsOneWay = true)]
+        void RegistrarJugador();
+    }
+}
