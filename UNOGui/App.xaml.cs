e53029358124dca43e2ae5f5a0dd3e35775baf0f
SammyGCh <53235299+SammyGCh@users.noreply.github.com>
2020-12-11 00:49:41 -0600

    Inicio de internacionalización
    Se modificó la pagina Login, Menu principal, Inicio, Mi perfil

--- UNOGui/App.xaml.cs
+++ UNOGui/App.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows;
 
 namespace UNOGui
@@ -13,5 +13,9 @@ namespace UNOGui
     /// </summary>
     public partial class App : Application
     {
+        App()
+        {
+            Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("en");
+        }
     }
 }
